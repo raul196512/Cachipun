3af5d76e01dd10d5bf90356405dd7736199dcce0
Raúl Castro <castroherreraraul@gmail.com>
2024-02-27 23:52:52 -0300

    ingreso botón de número de juegos

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,27 @@ function play(userChoice) {
 
     document.getElementById('result').innerHTML = 'Elegiste ' + userChoice + '. La computadora eligió ' + computerChoice + '. ' + result;
 }
+let contador = 0;
+let limiteDeOpcionesParaJugar = 10; // Número máximo de opciones para jugar
+
+function incrementarContador() {
+  if (contador < limiteDeOpcionesParaJugar) {
+    contador++;
+    actualizarContador();
+  } else {
+    alert("¡Se ha alcanzado el límite de opciones para jugar!");
+  }
+}
+
+
+//Resetear el contador
+function resetearContador() {
+    contador = 0;
+    actualizarContador();
+}
+
+
+function actualizarContador() {
+    let contadorElemento = document.getElementById('contador');
+    contadorElemento.textContent = contador;
+}
